f195c8138682230a2d77535b19036afb87f6d8d1
Valimayanavan <valimayanavan@gmail.com>
2024-11-04 05:37:14 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -4,69 +4,71 @@
 void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
+    int i,j,k;
+    int L[10];
+    int R[10]; 
 
-    int L[n1], R[n2]; // Temporary arrays for left and right subarrays
+    for ( i = 0; i < n1; i++)
+	L[i] = arr[l + i];
+    for ( j = 0; j < n2; j++)
+	R[j] = arr[m + 1 + j];
 
-    for (int i = 0; i < n1; i++) 
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++) 
-        R[j] = arr[m + 1 + j];
+    i = 0, j = 0, k = l;
 
-    int i = 0, j = 0, k = l;
-
-    // Merge the two halves back into arr[]
+   
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+	if (L[i] <= R[j]) {
+	    arr[k] = L[i];
+	    i++;
+	} else {
+	    arr[k] = R[j];
+	    j++;
+	}
+	k++;
     }
 
-    // Copy remaining elements of L[], if any
+   
     while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
+	arr[k] = L[i];
+	i++;
+	k++;
     }
 
-    // Copy remaining elements of R[], if any
+   
     while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+	arr[k] = R[j];
+	j++;
+	k++;
     }
 }
 
 void mergeSort(int arr[], int l, int r) {
     if (l < r) {
-        int m = l + (r - l) / 2;
+	int m = l + (r - l) / 2;
 
-        mergeSort(arr, l, m);    // Sort the first half
-        mergeSort(arr, m + 1, r); // Sort the second half
-        merge(arr, l, m, r);      // Merge the two halves
+	mergeSort(arr, l, m);    
+	mergeSort(arr, m + 1, r); 
+	merge(arr, l, m, r);      
     }
 }
 
 void printArray(int A[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", A[i]);
+    int i;
+    for (i = 0; i < size; i++)
+	printf("%d ", A[i]);
     printf("\n");
 }
 
 void main() {
-    int n;
+    int n,i,arr[20];
   clrscr();
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
-    int arr[n];
+
     printf("Enter %d elements of the array:\n", n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    for ( i = 0; i < n; i++)
+	scanf("%d", &arr[i]);
 
     printf("Given array is: \n");
     printArray(arr, n);
